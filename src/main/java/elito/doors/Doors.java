83072ed3ac422593fce78beccc9ca7db7d838737
ElitoSear <elitogamer100@gmail.com>
2025-03-17 12:47:29 -0600

    Added command execution from items.

--- src/main/java/elito/doors/Doors.java
+++ src/main/java/elito/doors/Doors.java
@@ -2,26 +2,68 @@ package elito.doors;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.block.ShapeContext;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.CustomModelDataComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.decoration.DisplayEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.RaycastContext;
-import static net.minecraft.server.command.CommandManager.*;
-import net.minecraft.server.command.SummonCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
+import static net.minecraft.server.command.CommandManager.literal;
 
 public class Doors implements ModInitializer {
 
+    String MOD_ID = "doors";
+    Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
+
+
+        UseItemCallback.EVENT.register((player, world, hand) -> {
+            if (!world.isClient()) {
+                ItemStack stack = player.getStackInHand(hand);
+
+                if (stack.contains(DataComponentTypes.CUSTOM_DATA)) {
+                    NbtCompound nbt = Objects.requireNonNull(stack.get(DataComponentTypes.CUSTOM_DATA)).copyNbt();
+
+                    String command = nbt.getString("command");
+
+                    if (nbt.contains("command")) {
+                        CommandManager commandManager = Objects.requireNonNull(player.getServer()).getCommandManager();
+                        ServerCommandSource commandSource = new ServerCommandSource(
+                                Objects.requireNonNull(player.getServer()),
+                                player.getPos(),
+                                player.getRotationClient(),
+                                world instanceof ServerWorld ? (ServerWorld) world : null,
+                                4,
+                                player.getName().getString(),
+                                player.getDisplayName(),
+                                player.getServer(),
+                                player
+                        );
+                        commandManager.executeWithPrefix(commandSource, command);
+
+                        return ActionResult.SUCCESS;
+                    }
+                }
+
+            }
+            return ActionResult.PASS;
+        });
 
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("doors")
                 .then(
@@ -34,16 +76,22 @@ public class Doors implements ModInitializer {
                                                             ServerWorld world = source.getWorld();
 
                                                             Vec3d origin = source.getPosition();
-                                                            Vec3d destination = origin.add(Vec3d.fromPolar(source.getRotation()).add(128));
+                                                            Vec2f rotation = source.getRotation();
 
-                                                            BlockHitResult blockHitResult = world.raycast(new RaycastContext(origin, destination, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, ShapeContext.absent()));
-                                                            Vec3d on = blockHitResult.getPos();
+                                                            BlockHitResult hit = Raycast.cast(origin, rotation, world);
 
-                                                            DisplayEntity.ItemDisplayEntity seekEye = SummonCommand.summon(context, )
+                                                            Vec3d on = hit.getPos();
+                                                            Vec2f newRotation = Raycast.getRotationWhenCollided(hit.getSide());
+
+                                                            on = Raycast.moveForward(on, newRotation, 0.05);
+
+                                                            DisplayEntity.ItemDisplayEntity seekEye = new DisplayEntity.ItemDisplayEntity(EntityType.ITEM_DISPLAY, world);
+                                                            seekEye.updatePositionAndAngles(on.x, on.y, on.z, newRotation.x, newRotation.y);
+                                                            world.spawnEntity(seekEye);
+
                                                             ItemStack item = new ItemStack(Items.STICK);
                                                             item.set(DataComponentTypes.ITEM_MODEL, Identifier.of("doors:entity/seek/eye"));
                                                             seekEye.setItemStack(item);
-                                                            seekEye.setPosition(on);
                                                             seekEye.addCommandTag("seek_eye");
                                                             return 1;
                                                         }
---
+++ src/main/java/elito/doors/Raycast.java
@@ -0,0 +1,43 @@
+package elito.doors;
+
+import net.minecraft.block.ShapeContext;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
+
+public class Raycast {
+
+    public static double maximumDistance = 128;
+
+    public static BlockHitResult cast(Vec3d origin, Vec2f rotation, ServerWorld world){
+
+        Vec3d rotationVector = Vec3d.fromPolar(rotation);
+        Vec3d rotationVectorExtended = rotationVector.multiply(Raycast.maximumDistance);
+
+        Vec3d destination = origin.add(rotationVectorExtended);
+
+        return world.raycast(new RaycastContext(origin, destination, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, ShapeContext.absent()));
+    }
+
+    public static Vec2f getRotationWhenCollided(Direction direction) {
+
+        return switch (direction.getName()) {
+            case "south" -> new Vec2f(0, 0);
+            case "east" -> new Vec2f(-90, 0);
+            case "west" -> new Vec2f(90, 0);
+            case "up" -> new Vec2f(0, -90);
+            case "down" -> new Vec2f(0, 90);
+            default -> new Vec2f(-180, 0);
+        };
+    }
+
+    public static Vec3d moveForward(Vec3d position, Vec2f rotation, double distance) {
+
+        Vec3d view = Vec3d.fromPolar(rotation);
+        return position.add(view.multiply(distance));
+
+    }
+}
